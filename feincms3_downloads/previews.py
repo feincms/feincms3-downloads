b999e93d096fecbf10580603fc35c24b6d677631
Matthias Kestenholz <mk@feinheit.ch>
2018-09-18 13:29:20 +0200

    Avoid abbreviations

--- feincms3_downloads/previews.py
+++ feincms3_downloads/previews.py
@@ -10,11 +10,11 @@ def preview_as_jpeg(path, *, resize="300x300>"):
         if path.lower().endswith(".pdf"):
             cmd.extend(["-background", "white", "-alpha", "remove"])
 
-        cmd.extend(["%s[0]" % path, "%s/pre.jpg" % directory])
+        cmd.extend(["%s[0]" % path, "%s/preview.jpg" % directory])
 
         # print(cmd)
         ret = subprocess.call(cmd, env={"PATH": "/usr/local/bin:/usr/bin:/bin"})
 
         if ret == 0:
-            with io.open("%s/pre.jpg" % directory, "rb") as f:
+            with io.open("%s/preview.jpg" % directory, "rb") as f:
                 return f.read()
