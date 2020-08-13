6c3b5266a48ac1d784d1680abf95cd88b002188c
Matthias Kestenholz <mk@feinheit.ch>
2020-08-13 22:20:03 +0200

    Drop Python<3.6 and Django<2.2

--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -1,16 +1,16 @@
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from django.contrib import admin
 
 from testapp import views
 
 
 articles_urlpatterns = (
-    [url(r"^(?P<pk>[0-9]+)/$", views.article_detail, name="article")],
+    [re_path(r"^(?P<pk>[0-9]+)/$", views.article_detail, name="article")],
     "articles",
 )
 
 
 urlpatterns = [
-    url(r"^admin/", admin.site.urls),
-    url(r"", include(articles_urlpatterns)),
+    re_path(r"^admin/", admin.site.urls),
+    re_path(r"", include(articles_urlpatterns)),
 ]
